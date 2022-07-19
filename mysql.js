06a0eb13418885f4c0beb3222866bdc8021e4159
zhoumowubuzuoye <www.1078637049@qq.com>
2022-07-19 14:52:19 +0800

    session

--- mysql.js
+++ mysql.js
@@ -1,18 +1,72 @@
 /*
  * @Author: xiewenhao
  * @Date: 2022-07-18 17:03:09
- * @LastEditTime: 2022-07-18 17:29:01
+ * @LastEditTime: 2022-07-19 14:23:15
  * @Description: 
  */
+// const mysql = require('mysql')
+// const db = mysql.createPool({
+//     host:'127.0.0.1',
+//     user:'root',
+//     password:'admin123',
+//     database:'my_db_01'
+// })
+// const dbStr = 'select * from users'
+// db.query(dbStr,(err,res)=>{
+//     console.log(err);
+//     err??console.log(res);
+// })
+
+// const mysql = require('mysql')
+// const db = mysql.createPool({
+//     host:'127.0.0.1',
+//     user:'root',
+//     password:'admin123',
+//     database:'my_db_01'
+// })
+// const sqlStr = 'select * from users where id < ?'
+// db.query(sqlStr,10,(err,res)=>{
+//     err??console.log(res);
+// })
+
+// const mysql = require('mysql')
+// const db = mysql.createPool({
+//     host: '127.0.0.1',
+//     user: 'root',
+//     database: 'my_db_01',
+//     password: 'admin123'
+// })
+// const user = {
+//     username:'ww',
+//     password:'123321'
+// }
+// const sqlStr = 'insert into users set ?'
+// db.query(sqlStr,user, (err, res) => {
+//     err ?? console.log(res)
+// })
+//
+
+const {
+    log
+} = require('console')
 const mysql = require('mysql')
-const sqpStr = 'insert into users set ?'
-const user = {username:'ww',password:1234567}
 const db = mysql.createPool({
-    host:'127.0.0.1',
-    user:'root',
-    password:'admin123',
-    database:'my_db_01'
+    hose: '127.0.0.1',
+    user: 'root',
+    password: 'admin123',
+    database: 'my_db_01'
 })
-db.query(sqpStr,user,(err,result)=>{
-  err??console.log(result);
+// const strSql = 'update  users set? where id=? '
+// const user = {
+//     id: 12,
+//     password: '22222',
+//     username: 'wwwww'
+// }
+// db.query(strSql, [user, user.id], (err, res) => {
+//     err ? console.log(err) : console.log(res);
+// })
+
+const strSql = 'delete from users where id=?'
+db.query(strSql, 12, (err, res) => {
+    err ? console.log(err) : console.log(res);
 })
